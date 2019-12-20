d6c6edf27adc423663188ebc266c914132f84914
otreblan <otreblain@gmail.com>
2019-12-20 17:02:28 -0500

    Now with base message

--- include/clsp/messages/message.hpp
+++ include/clsp/messages/message.hpp
@@ -1,8 +1,22 @@
 #ifndef CLSP_MESSAGES_MESSAGE_H
 #define CLSP_MESSAGES_MESSAGE_H
 
+#include <string>
+
 namespace clsp
 {
+
+using namespace std;
+
+struct Message
+{
+
+	const static string jsonrpcKey;
+	const static string jsonrpc;
+
+	Message();
+	virtual ~Message();
+};
 
 }
 
--- src/messages/message.cpp
+++ src/messages/message.cpp
@@ -1,1 +1,11 @@
 #include <clsp/messages/message.hpp>
+
+namespace clsp
+{
+
+using namespace std;
+
+const string Message::jsonrpcKey = "jsonrpc";
+const string Message::jsonrpc    = "2.0";
+
+}
