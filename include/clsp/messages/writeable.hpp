79a04b4a93d948d81ffded42f51c412e5d068566
otreblan <otreblain@gmail.com>
2019-12-20 07:32:01 -0500

    Added placeholder for the writing interface

---
+++ include/clsp/messages/writeable.hpp
@@ -0,0 +1,9 @@
+#ifndef CLSP_MESSAGES_WRITEABLE_H
+#define CLSP_MESSAGES_WRITEABLE_H
+
+namespace clsp
+{
+
+}
+
+#endif /* CLSP_MESSAGES_WRITEABLE_H */
---
+++ src/messages/writeable.cpp
@@ -0,0 +1,1 @@
+#include <clsp/messages/writeable.hpp>
