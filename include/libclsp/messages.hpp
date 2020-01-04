110bcfd0890e0321a60eaadfbe1f20b8e5f6a6b4
otreblan <otreblain@gmail.com>
2020-01-04 09:35:53 -0500

    DidChangeWorkspaceFolders added

--- include/libclsp/messages.hpp
+++ include/libclsp/messages.hpp
@@ -18,6 +18,7 @@
 
 #include <libclsp/messages/cancelParams.hpp>
 #include <libclsp/messages/diagnostic.hpp>
+#include <libclsp/messages/didChangeWorkspaceFolders.hpp>
 #include <libclsp/messages/documentFilter.hpp>
 #include <libclsp/messages/fileResourceChanges.hpp>
 #include <libclsp/messages/jsonTypes.hpp>
