2865e2829a85e67085dba7e2b7609a78e01485a5
otreblan <otreblain@gmail.com>
2020-02-11 18:10:31 -0500

    More writing #2
    Non writeable:
    TextDocumentPositionParams

--- include/libclsp/types/textDocumentPosition.hpp
+++ include/libclsp/types/textDocumentPosition.hpp
@@ -54,6 +54,7 @@ public:
 
 	//=======================================================================//
 
+	// No writing
 
 	TextDocumentPositionParams(TextDocumentIdentifier textDocument,
 		Position position);
